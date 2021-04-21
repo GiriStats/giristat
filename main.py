7f82c2fe5281f217aec0e05a2db3375c516381b5
Aleksei Dudchenko <aleksei.dudchenko@med.uni-heidelberg.de>
2021-04-21 10:51:50 +0200

    add draw_median_and_mean(df, discipline, year, save, dpi)

--- main.py
+++ main.py
@@ -5,4 +5,10 @@ import plots.median_mean as mm
 
 df = perd.get_df()
 
-mm.draw_median_and_mean(df, True, 100)
+
+mm.draw_median_and_mean(df, discipline='Сумма дв-рья', year=2020,  save=False, dpi=100)
+
+# mm.draw_median_and_mean(df, discipline='Толчок', year=2020,  save=False, dpi=100)
+
+# mm.draw_median_and_mean(df, discipline='Рывок (сумма)', year=2020,  save=False, dpi=100)
+
